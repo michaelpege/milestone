5c2b0b6fbc04ccfcb2c2af15dc37ac388697e99a
pege <mpg066@gmail.com>
2020-08-04 22:01:47 +0700

    penambahan library2

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from "react";
-import { Text, View, SafeAreaView } from "react-native";
+import {
+  Text,
+  View,
+  SafeAreaView,
+  Button,
+  TouchableHighlight,
+} from "react-native";
 import { Biodata } from "./components/Biodata";
 import { FoodEaten } from "./components/Food";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import DateTimePicker from "@react-native-community/datetimepicker";
+import Modal from "react-native-modal";
 
 class App extends React.Component {
+  state = {
+    modalActive: false,
+    text: "",
+  };
+  toggleModal(modalActive) {
+    this.setState({ modalActive: modalActive });
+  }
+  pushText() {
+    let text = this.state.text;
+    text += "cekrek ";
+    this.setState({ text });
+  }
   render() {
     let data = {
       nama: "Alwan",
@@ -11,9 +32,23 @@ class App extends React.Component {
     };
     return (
       <SafeAreaView>
-        <Text>Open up App.js to start working on your app!</Text>
         <Biodata data={data} />
         <FoodEaten />
+        <Text>{`Your log : ${this.state.text}`}</Text>
+        <Modal
+          isVisible={this.state.modalActive}
+          onBackdropPress={() => this.toggleModal(false)}
+        >
+          <Text>YEY MODALNYA KEBUKA!</Text>
+          <Button
+            onPress={() => this.toggleModal(false)}
+            title={"Tutup Modal"}
+          />
+        </Modal>
+        <Button onPress={() => this.toggleModal(true)} title={"Buka Modal"} />
+        <TouchableHighlight onPress={() => this.pushText()}>
+          <MaterialCommunityIcons name='camera' color={"#000000"} size={26} />
+        </TouchableHighlight>
       </SafeAreaView>
     );
   }
--- components/biodata.js
+++ components/biodata.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Formik } from "formik";
+import * as yup from "yup";
 
 class Biodata extends React.Component {
   render() {
