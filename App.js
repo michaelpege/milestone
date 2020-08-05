d0780324ef3edb5e85e188036611db6b17606776
Jauhar-Wibisono <jauhar.wibisono@gmail.com>
2020-08-06 00:40:38 +0700

    biodata +modal copas docs Wibi

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, {useState} from "react";
 import {
   Text,
   View,
   SafeAreaView,
   Button,
   TouchableHighlight,
+  StyleSheet,
 } from "react-native";
 import { Biodata } from "./components/Biodata";
 import { FoodEaten } from "./components/Food";
@@ -12,45 +13,46 @@ import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityI
 import DateTimePicker from "@react-native-community/datetimepicker";
 import Modal from "react-native-modal";
 
-class App extends React.Component {
-  state = {
-    modalActive: false,
-    text: "",
-  };
-  toggleModal(modalActive) {
-    this.setState({ modalActive: modalActive });
-  }
-  pushText() {
-    let text = this.state.text;
-    text += "cekrek ";
-    this.setState({ text });
-  }
-  render() {
-    let data = {
-      nama: "Alwan",
-      umur: 19,
-    };
-    return (
-      <SafeAreaView>
-        <Biodata data={data} />
-        <FoodEaten />
-        <Text>{`Your log : ${this.state.text}`}</Text>
-        <Modal
-          isVisible={this.state.modalActive}
-          onBackdropPress={() => this.toggleModal(false)}
-        >
-          <Text>YEY MODALNYA KEBUKA!</Text>
-          <Button
-            onPress={() => this.toggleModal(false)}
-            title={"Tutup Modal"}
-          />
-        </Modal>
-        <Button onPress={() => this.toggleModal(true)} title={"Buka Modal"} />
-        <TouchableHighlight onPress={() => this.pushText()}>
-          <MaterialCommunityIcons name='camera' color={"#000000"} size={26} />
-        </TouchableHighlight>
-      </SafeAreaView>
-    );
-  }
+/*
+class AddBill extends React.Component{
+	constructor(props) {
+		super(props);
+		this.changeDate = this.changeDate.bind(this);
+		this.toggleDatePicker = this.toggleDatePicker.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
+		this.switchLendeeMode = this.switchLendeeMode.bind(this);
+		this.addData = this.addData.bind(this);
+		this.toggleSelect = this.toggleSelect.bind(this);
+		this.deleteLendee = this.deleteLendee.bind(this);
+		this.switchBillFormat = this.switchBillFormat.bind(this);
+		this.onModalSubmit = this.onModalSubmit.bind(this);
+	}
+	state = {
+		date : today,
+		dateFull : {y,m,d},
+		datePicker : false,
+		dateObj : dateObj,
+		modalActive : false,
+		modalSwitch : "Add New Lendee",
+		dataNew : [],
+		dataExisting : [],
+		newSelected : [],
+		existingSelected : [],
+		lendeeId : [],
+		lendee_data : this.props.lendee_data,
+		billFormat : "Equal",
+	};	
 }
+*/
+
+class App extends React.Component {
+	render(){
+		return(
+			<View>
+				<Biodata/>
+			</View>
+		);
+	}
+}
+
 export default App;
--- components/biodata.js
+++ components/biodata.js
@@ -1,17 +1,153 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, {Component} from "react";
+import { StyleSheet, Text, View, Modal, TouchableHighlight } from "react-native";
 import { Formik } from "formik";
 import * as yup from "yup";
+import DateTimePicker from "@react-native-community/datetimepicker";
+
+class AddBill extends React.Component{
+	constructor(props) {
+		super(props);
+		this.changeDate = this.changeDate.bind(this);
+		this.toggleDatePicker = this.toggleDatePicker.bind(this);
+		this.toggleModal = this.toggleModal.bind(this);
+		this.switchLendeeMode = this.switchLendeeMode.bind(this);
+		this.addData = this.addData.bind(this);
+		this.toggleSelect = this.toggleSelect.bind(this);
+		this.deleteLendee = this.deleteLendee.bind(this);
+		this.switchBillFormat = this.switchBillFormat.bind(this);
+		this.onModalSubmit = this.onModalSubmit.bind(this);
+	}
+	state = {
+		date : today,
+		dateFull : {y,m,d},
+		datePicker : false,
+		dateObj : dateObj,
+		modalActive : false,
+		modalSwitch : "Add New Lendee",
+		dataNew : [],
+		dataExisting : [],
+		newSelected : [],
+		existingSelected : [],
+		lendeeId : [],
+		lendee_data : this.props.lendee_data,
+		billFormat : "Equal",
+	};
+	/*
+	render(){
+		return(
+
+		);
+	}
+	*/
+}
 
 class Biodata extends React.Component {
-  render() {
-    let bio = this.props.data;
-    return (
-      <View>
-        <Text>{`Halo aku ${bio.nama}. Umurku ${bio.umur}`}</Text>
-      </View>
-    );
-  }
+	constructor(props) {
+		super(props);
+	}
+	state = {
+		bio : {
+			gender : "",
+			birthDate : "",
+			weight : "",
+			height : "",
+			bmi : "",
+			allergies : [],
+		},
+		modalVisible : false
+	};
+	setModalVisible = (visible) => {
+		this.setState({modalVisible : visible});
+	}
+	render() {
+		let bio = this.state.bio
+		const {modalVisible} = this.state.modalVisible;
+		return (
+			<View style={styles.centeredView}>
+				<Text>
+					{`Gender : ${bio.gender}\n
+					Birthdate : ${bio.birthDate}\n
+					Weight : ${bio.weight}\n
+					Height : ${bio.height}\n
+					BMI : ${bio.bmi}\n
+					Allergies : ${bio.allergies}`}
+				</Text>
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+				>
+					<View style={styles.centeredView}>
+						<View style={styles.modalView}>
+							<Text style={styles.modalText}>
+								{`Gender : ${bio.gender}\n
+								Birthdate : ${bio.birthDate}\n
+								Weight : ${bio.weight}\n
+								Height : ${bio.height}\n
+								BMI : ${bio.bmi}\n
+								Allergies : ${bio.allergies}`}
+							</Text>
+							<TouchableHighlight
+								style={styles.openButton}
+								onPress{...()=>{
+									this.setModalVisible(!modalVisible);
+								}}
+							>
+								<Text style={styles.textStyle}>{`Close`}</Text>
+							</TouchableHighlight>
+						</View>
+					</View>
+				</Modal>
+				<TouchableHighlight
+					style={styles.openButton}
+					onPress{...()=>{
+						this.setModalVisible(true);
+					}}
+				>
+					<Text style={styles.textStyle}>{`Edit`}</Text>
+				</TouchableHighlight>
+			</View>
+		);
+	}
 }
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5
+  },
+  openButton: {
+    backgroundColor: "#F194FF",
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
+});
 
 export { Biodata };
