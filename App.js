f63a491574cff6a20387c2efc313e1048962ca11
pege <mpg066@gmail.com>
2020-08-07 21:41:30 +0700

    tambahan row biodata alergy

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ import {
   TouchableHighlight,
   StyleSheet,
 } from "react-native";
-import { Biodata } from "./components/Biodata";
+import { Biodata, BiodataModal } from "./components/Biodata";
 import { FoodEaten } from "./components/Food";
 import { Statistic } from "./components/statistic";
 import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
@@ -15,47 +15,62 @@ import DateTimePicker from "@react-native-community/datetimepicker";
 import Modal from "react-native-modal";
 import { ProgressBar } from "./components/statistic";
 
-/*
-class AddBill extends React.Component{
-	constructor(props) {
-		super(props);
-		this.changeDate = this.changeDate.bind(this);
-		this.toggleDatePicker = this.toggleDatePicker.bind(this);
-		this.toggleModal = this.toggleModal.bind(this);
-		this.switchLendeeMode = this.switchLendeeMode.bind(this);
-		this.addData = this.addData.bind(this);
-		this.toggleSelect = this.toggleSelect.bind(this);
-		this.deleteLendee = this.deleteLendee.bind(this);
-		this.switchBillFormat = this.switchBillFormat.bind(this);
-		this.onModalSubmit = this.onModalSubmit.bind(this);
-	}
-	state = {
-		date : today,
-		dateFull : {y,m,d},
-		datePicker : false,
-		dateObj : dateObj,
-		modalActive : false,
-		modalSwitch : "Add New Lendee",
-		dataNew : [],
-		dataExisting : [],
-		newSelected : [],
-		existingSelected : [],
-		lendeeId : [],
-		lendee_data : this.props.lendee_data,
-		billFormat : "Equal",
-	};	
-}
-*/
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setModalVisible = this.setModalVisible.bind(this);
+  }
   state = {
     modalActive: false,
     text: "",
+    modalComponent: "Biodata",
+    modalVisible: false,
+    bio: {
+      gender: "Male",
+      birthDate: "01/01/2000",
+      weight: "57",
+      height: "170",
+      bmi: "20.1",
+      allergies: ["Nuts", "Seafood"],
+    },
   };
+  setModalVisible(visible, type) {
+    type = type == "" || type == undefined ? this.state.modalComponent : type;
+    this.setState({ modalVisible: visible, modalComponent: type });
+    console.log("Masuk");
+    console.log(visible, type);
+  }
   render() {
+    let modalComponent = "";
+    switch (this.state.modalComponent) {
+      case "Biodata":
+        modalComponent = (
+          <BiodataModal
+            setModalVisible={this.setModalVisible}
+            bio={this.state.bio}
+            visible={this.state.modalVisible}
+          />
+        );
+        break;
+    }
     return (
-      <SafeAreaView>
-        <Biodata />
+      <SafeAreaView
+        style={{
+          width: "100%",
+          height: "100%",
+          flex: 1,
+          flexDirection: "column",
+          alignItems: "center",
+          padding: 20,
+        }}
+      >
+        <Biodata bio={this.state.bio} setModalVisible={this.setModalVisible} />
+        <Modal
+          isVisible={this.state.modalVisible}
+          onBackdropPress={() => this.setModalVisible(false)}
+        >
+          {modalComponent}
+        </Modal>
         <Statistic />
       </SafeAreaView>
     );
--- components/biodata.js
+++ components/biodata.js
@@ -1,33 +1,25 @@
-import React, { Component } from "react";
-import { StyleSheet, Text, View, TouchableHighlight } from "react-native";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableHighlight,
+  FlatList,
+} from "react-native";
 import Modal from "react-native-modal";
 import { dimension, color } from "../assets/style";
-import { Formik } from "formik";
-import * as yup from "yup";
 import DateTimePicker from "@react-native-community/datetimepicker";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
 
 class Biodata extends React.Component {
   constructor(props) {
     super(props);
-    this.setModalVisible = this.setModalVisible.bind(this);
   }
 
   state = {
-    bio: {
-      gender: "Male",
-      birthDate: "01/01/2000",
-      weight: "57",
-      height: "170",
-      bmi: "20.1",
-      allergies: ["Nuts", "Seafood"],
-    },
+    bio: this.props.bio,
     modalVisible: false,
   };
-
-  setModalVisible(visible) {
-    this.setState({ modalVisible: visible });
-    console.log("Masuk");
-  }
 
   render() {
     let bio = this.state.bio;
@@ -57,26 +49,27 @@ class Biodata extends React.Component {
               Allergies
             </Text>
           </View>
-          <View style={[styles.bioRow, { padding: 10 }]}>
-            <View style={styles.innerBox}></View>
+          <View style={[styles.bioRow]}>
+            <View style={styles.innerBox}>
+              <FlatList
+                data={bio.allergies}
+                renderItem={({ item }) => (
+                  <Text style={{ width: "33%", margin: 10 }}>{item}</Text>
+                )}
+                numColumns={3}
+                keyExtractor={(item) => item}
+              />
+            </View>
           </View>
           <View>
             <TouchableHighlight
               style={[styles.buttonMin, { position: "absolute", right: 0 }]}
-              onPress={() => this.setModalVisible(true)}
+              onPress={() => this.props.setModalVisible(true, "Biodata")}
             >
               <Text style={styles.textStyle}>{`Edit`}</Text>
             </TouchableHighlight>
           </View>
         </View>
-
-        <Modal isVisible={this.state.modalVisible}>
-          <BiodataModal
-            setModalVisible={this.setModalVisible}
-            bio={bio}
-            visible={this.state.modalVisible}
-          />
-        </Modal>
       </View>
     );
   }
@@ -85,7 +78,12 @@ class Biodata extends React.Component {
 class BiodataModal extends React.Component {
   constructor(props) {
     super(props);
+    this.handleChange = this.handleChange.bind(this);
   }
+  state = {
+    bio: this.props.bio,
+  };
+  handleChange() {}
   render() {
     let bio = this.props.bio;
     return (
@@ -101,10 +99,11 @@ class BiodataModal extends React.Component {
           </Text>
           <TouchableHighlight
             style={styles.openButton}
-            onPress={() => this.props.setModalVisible(!this.props.visible)}
+            onPress={() => this.props.setModalVisible(false, "Biodata")}
           >
             <Text style={styles.textStyle}>{`Close`}</Text>
           </TouchableHighlight>
+          <Allergies data={["Nuts", "Dairy", "Seafood", "Eggs", "Wheat"]} />
         </View>
       </View>
     );
@@ -122,7 +121,100 @@ class BioItem extends React.Component {
   }
 }
 
+class Allergies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.addAllergy = this.addAllergy.bind(this);
+    this.deleteAllergy = this.deleteAllergy.bind(this);
+  }
+  state = {
+    database: this.props.data,
+    selected: [],
+  };
+  addAllergy(name) {
+    let selected = this.state.selected;
+    if (!selected.includes(name)) {
+      selected.push(name);
+    }
+    this.setState({ selected });
+  }
+  deleteAllergy(name) {
+    let selected = this.state.selected;
+    if (selected.includes(name)) {
+      selected = selected.filter((row) => row != name);
+    }
+    this.setState({ selected });
+  }
+  render() {
+    let { selected, database } = this.state;
+    let notSelected = database.filter((row) => !selected.includes(row));
+    let selectedAllergies = selected.map((row) => (
+      <AllergyRow
+        name={row}
+        onPress={this.deleteAllergy}
+        handlePress={row}
+        selected={true}
+      />
+    ));
+    let notSelectedAllergies = notSelected.map((row) => (
+      <AllergyRow
+        name={row}
+        onPress={this.addAllergy}
+        handlePress={row}
+        selected={false}
+      />
+    ));
+    return (
+      <View>
+        <Text>Allergies</Text>
+        <View style={styles.allergyContainer}>{selectedAllergies}</View>
+        <Text>Add Allergies</Text>
+        <View style={styles.allergyContainer}>{notSelectedAllergies}</View>
+      </View>
+    );
+  }
+}
+
+class AllergyRow extends React.Component {
+  render() {
+    let icon = this.props.selected ? (
+      <MaterialCommunityIcons name='minus' size={24} color={color.p_red} />
+    ) : (
+      <MaterialCommunityIcons name='plus' size={24} color={color.p_teal} />
+    );
+    return (
+      <View style={styles.allergyRow}>
+        <Text style={styles.allergyName}>{this.props.name}</Text>
+        <TouchableHighlight
+          onPress={() => this.props.onPress(this.props.handlePress)}
+        >
+          {icon}
+        </TouchableHighlight>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
+  allergyContainer: {
+    width: "200",
+    height: 100,
+    borderRadius: 4,
+    borderColor: color.p_teal,
+  },
+  allergyRow: {
+    width: "100%",
+    backgroundColor: color.p_lightBlue,
+    height: 30,
+    flexDirection: "row",
+  },
+  allergyName: {
+    flex: 3,
+  },
+  allergyButton: {
+    flex: 1,
+    backgroundColor: color.f_gray,
+  },
   centeredView: {
     flex: 1,
     justifyContent: "center",
@@ -173,6 +265,7 @@ const styles = StyleSheet.create({
     height: 200,
     backgroundColor: color.p_lightBlue,
     borderRadius: 8,
+    justifyContent: "space-between",
   },
   modalView: {
     margin: 20,
@@ -206,4 +299,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export { Biodata };
+export { Biodata, BiodataModal };
